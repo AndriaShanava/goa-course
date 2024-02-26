5d01c6d6eea5fbed935828091c60ad296522b85b
AndriaShanava <jemalatyabladze2000@gmail.com>
2024-02-26 19:16:10 +0400

    eleventh commit

---
+++ day07/homework/lesson7.py
@@ -0,0 +1,14 @@
+balance = 780
+print(type(balance))
+
+balance = "780"
+print(type(balance))
+
+balance = 234.3
+print(type(balance))
+
+city = "berlin"
+print(type(city))
+
+nomber1 = input(1,2,3,4,5,6,7,8)
+print(input()/8)
