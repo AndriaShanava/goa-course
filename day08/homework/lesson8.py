3404ba2e067e63a2f228b1c2edc73ebfc007580d
AndriaShanava <jemalatyabladze2000@gmail.com>
2024-02-28 21:55:59 +0400

    twelfth commit

---
+++ day08/homework/lesson8.py
@@ -0,0 +1,4 @@
+user_age = input("How old are you?: ")
+user_ageinfuture = input("How many years would you like to travel through time?: ")
+user_ageinfuture2 = input("How old will you be after time travel?: ")
+print(" my agee is " + user_age + " I will be after time travel " +  user_ageinfuture  + " years " + " I'll be after the time travelers " + user_ageinfuture2 + " years ")
