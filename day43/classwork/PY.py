794e7635d2d73b3d7a999cab7ce68caedd2aebda
AndriaShanava <156942481+AndriaShanava@users.noreply.github.com>
2024-07-01 23:07:52 +0400

    commit

---
+++ day43/classwork/PY.py
@@ -0,0 +1,30 @@
+# for i in range(21):
+#     print(i)
+
+# Print the first 10 natural numbers
+# for i in range(1, 11):
+#     print(i)
+
+# # Print even numbers from 0 to 100
+# print("Even numbers from 0 to 100:")
+# for i in range(0, 101):
+#     if i % 2 == 0:
+#         print(i)
+
+# # Print odd numbers from 0 to 100
+# print("\nOdd numbers from 0 to 100:")
+# for i in range(0, 101):
+#     if i % 2 != 0:
+#         print(i)
+
+
+
+
+total_sum = 0
+
+# Use a for loop to iterate through numbers from 1 to 10
+for number in range(1, 11):
+    total_sum += number
+
+# Print the result
+print("The sum of numbers from 1 to 10 is:", total_sum)
