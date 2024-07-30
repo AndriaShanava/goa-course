8a86a0939e4d48b738bb9a7981a98f9cba32ac4f
AndriaShanava <156942481+AndriaShanava@users.noreply.github.com>
2024-07-30 13:42:12 +0400

    commit

---
+++ day48/homework/48py.py
@@ -0,0 +1,26 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.append(50)
+my_list.append(60)
+my_list.append(70)
+
+
+my_list[2], my_list[3] = my_list[3], my_list[2]
+
+
+large_number = 999999
+my_list.append(large_number)
+
+
+my_list.pop(5)   
+my_list.pop(0)   
+
+
+my_list[3] = 888888
+
+
+print(my_list)
