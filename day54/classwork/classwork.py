b75b9f4635558e48ff5276ffc7a903f670d92f8b
AndriaShanava <156942481+AndriaShanava@users.noreply.github.com>
2024-08-02 00:37:14 +0400

    commit

---
+++ day54/classwork/classwork.py
@@ -0,0 +1,13 @@
+def numbers(kentiluwi):
+
+    if (kentiluwi) % 2 ==0:
+       print("ლუწია")
+    
+    elif (kentiluwi) % 2 !=0:
+        print("კენტია")
+
+kentiluwi = int(input("შემოიტანეთ რიცხვი: "))
+
+numbers(kentiluwi)
+
+print(numbers)
